6f7a730ec5c278bcabc810a704bb94f8976ab9ae
Liu Zechu <liuzechu2013@gmail.com>
2020-03-13 18:14:36 +0800

    add basic network

---
+++ Relentless/Relentless/Model/Package.swift
@@ -0,0 +1,14 @@
+//
+//  Package.swift
+//  Relentless
+//
+//  Created by Liu Zechu on 13/3/20.
+//  Copyright © 2020 OurNameIs. All rights reserved.
+//
+
+import Foundation
+
+struct Package {
+    let index: Int
+    // something
+}

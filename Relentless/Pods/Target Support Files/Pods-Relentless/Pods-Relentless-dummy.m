be69d8b4fb9e0b4a8431a6306f588bec33f5e0cb
chowyiyin <yiyin.chow@gmail.com>
2020-03-15 19:38:07 +0800

    Remove untracked files

--- Relentless/Pods/Target Support Files/Pods-Relentless/Pods-Relentless-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Relentless : NSObject
-@end
-@implementation PodsDummy_Pods_Relentless
-@end
--- Relentless/Pods/Target Support Files/Pods-RelentlessTests/Pods-RelentlessTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RelentlessTests : NSObject
-@end
-@implementation PodsDummy_Pods_RelentlessTests
-@end

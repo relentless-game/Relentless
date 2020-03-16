73a30f59e8a1b83fe3ff8b0973245a282f2e735f
yaaanch <8232205+yaaanch@users.noreply.github.com>
2020-03-16 23:00:34 +0800

    Adds basic start, lobby, join views

--- Relentless/Relentless/UI/JoinViewController.swift
+++ Relentless/Relentless/UI/JoinViewController.swift
@@ -6,4 +6,77 @@
 //  Copyright © 2020 OurNameIs. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class JoinViewController: UIViewController, UITextFieldDelegate {
+
+    static var teamCodeCharacterLimit = 4
+    @IBOutlet private var teamCodeTextField: UITextField!
+    @IBOutlet private var joinButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        teamCodeTextField.smartInsertDeleteType = UITextSmartInsertDeleteType.no
+        teamCodeTextField.delegate = self
+    }
+
+    // Code obtained and modified from:
+    // https://stackoverflow.com/questions/12944789/allow-only-numbers-for-uitextfield-input
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        // Handle backspace/delete
+        guard !string.isEmpty else {
+            // Backspace detected, allow text change, no need to process the text any further
+            return true
+        }
+
+        // Input Validation
+        // Prevent invalid character input, if keyboard is numberpad
+        if textField.keyboardType == .numberPad {
+            // Check for invalid input characters
+            if CharacterSet(charactersIn: "0123456789").isSuperset(of: CharacterSet(charactersIn: string)) {
+                // Present alert so the user knows what went wrong
+//                presentAlert("This field accepts only numeric entries.")
+                // Invalid characters detected, disallow text change
+                return false
+            }
+        }
+
+        // Length Processing
+        // Need to convert the NSRange to a Swift-appropriate type
+        if let text = textField.text, let range = Range(range, in: text) {
+            let proposedText = text.replacingCharacters(in: range, with: string)
+            // Check proposed text length does not exceed max character count
+            guard proposedText.count <= JoinViewController.teamCodeCharacterLimit else {
+                // Present alert if pasting text
+                // easy: pasted data has a length greater than 1; who copy/pastes one character?
+                if string.count > 1 {
+                    // Pasting text, present alert so the user knows what went wrong
+//                    presentAlert("Paste failed: Maximum character count exceeded.")
+                }
+
+                // Character count exceeded, disallow text change
+                return false
+            }
+
+            // Only enable the OK/submit button if they have entered all numbers for the last four
+            // of their SSN (prevents early submissions/trips to authentication server, etc)
+            joinButton.isEnabled = (proposedText.count == JoinViewController.teamCodeCharacterLimit)
+        }
+
+        // Allow text change
+        return true
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "joinGame" {
+            let viewController = segue.destination as? LobbyViewController
+            guard let gameIdText = teamCodeTextField.text, viewController != nil else {
+                return
+            }
+            // viewControlelr is non-nil
+            viewController!.gameId = Int(gameIdText)
+        }
+    }
+}
--- Relentless/Relentless/UI/LobbyViewController.swift
+++ Relentless/Relentless/UI/LobbyViewController.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2020 OurNameIs. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LobbyViewController: UIViewController, UITextFieldDelegate {
+
+    static var nameCharacterLimit = 8
+    static var dummyName = "foobar"
+    var gameId: Int?
+    var userId: String?
+    var gameController: GameController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gameController = GameControllerManager()
+//        teamCodeTextField.smartInsertDeleteType = UITextSmartInsertDeleteType.no
+//        teamCodeTextField.delegate = self
+    }
+
+    func joinGame() {
+        guard let userId = userId, let gameId = gameId else {
+            return
+        }
+        let result = gameController?.joinGame(userId: userId, userName: LobbyViewController.dummyName, gameId: gameId)
+        assert(result ?? false)
+    }
+}

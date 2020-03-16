a63a77a7327f9dafac48eaa597c4c811033fa78f
yaaanch <8232205+yaaanch@users.noreply.github.com>
2020-03-16 22:59:51 +0800

    Adds basic start, join, lobby views

---
+++ Relentless/Relentless/UI/JoinViewController.swift
@@ -0,0 +1,9 @@
+//
+//  JoinViewController.swift
+//  Relentless
+//
+//  Created by Yanch on 16/3/20.
+//  Copyright © 2020 OurNameIs. All rights reserved.
+//
+
+import Foundation
---
+++ Relentless/Relentless/UI/LobbyViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LobbyViewController.swift
+//  Relentless
+//
+//  Created by Yanch on 16/3/20.
+//  Copyright © 2020 OurNameIs. All rights reserved.
+//
+
+import Foundation

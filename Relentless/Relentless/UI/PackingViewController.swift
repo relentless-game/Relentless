d2eeadcce7be3c664c4605d267daabf2b13ec873
yaaanch <8232205+yaaanch@users.noreply.github.com>
2020-03-19 23:05:20 +0800

    Fails to work packages

--- Relentless/Relentless/UI/PackingViewController.swift
+++ Relentless/Relentless/UI/PackingViewController.swift
@@ -12,8 +12,8 @@ class PackingViewController: UIViewController {
     var gameController: GameController?
 
     // todo: connect to storyboard
-    private weak var itemsCollectionView: UICollectionView!
-    private weak var categoryButton: UIButton!
+//    private weak var itemsCollectionView: UICollectionView!
+//    private weak var categoryButton: UIButton!
 
     // items will be updated when the category is changed
     var items: [Item]?
@@ -21,9 +21,51 @@ class PackingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        itemsCollectionView.delegate = self
-        itemsCollectionView.dataSource = self
+//        itemsCollectionView.delegate = self
+//        itemsCollectionView.dataSource = self
+//        drawPackages()
     }
+
+//    func drawPackages() {
+////        let packages = gameController.playerPackages
+//        var packages = [Package]()
+//        packages.append(Package(creator: "hi", packageNumber: 100, items: []))
+//        for package in packages {
+//            drawPackage(package)
+//        }
+//    }
+
+//    func drawPackage(_ package: Package) {
+//        let packageView = UILabel()
+//        packageView.text = String(package.packageNumber)
+//        packageView.isUserInteractionEnabled = true
+//        attachPackageTouchHandlers(to: packageView)
+////        packageStack.addSubview(packageView)
+//    }
+//
+//    func attachPackageTouchHandlers(to view: UIView) {
+//        let pan = UIPanGestureRecognizer(target: self, action: #selector(self.handlePackageDrag))
+//        view.addGestureRecognizer(pan)
+//    }
+//
+//    @objc func handlePackageDrag(_ recognizer: UIPanGestureRecognizer) {
+//        let translation = recognizer.translation(in: self.view)
+//        view.center = CGPoint(x: view.center.x + translation.x,
+//                              y: view.center.y + translation.y)
+//
+//        recognizer.setTranslation(CGPoint.zero, in: self.view)
+//
+////        if recognizer.state == .began {
+////            initialPegPoint = view.center
+////        }
+//        if recognizer.state == .ended {
+////            let moveSuccess = movePeg(view: view, to: view.center)
+////            if !moveSuccess {
+////                view.center = initialPegPoint
+////                return
+////            }
+//        }
+//    }
 
 }
 
@@ -45,4 +87,15 @@ UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
        return cell
     }
 
+}
+
+class PackageWrapper {
+    let package: Package
+    let view: UILabel
+
+    init(package: Package) {
+        self.package = package
+        self.view = UILabel()
+        view.text = String(package.packageNumber)
+    }
 }

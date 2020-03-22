de8a669440c10de7ccf313931cf04c3ae0bd2b76
chowyiyin <yiyin.chow@gmail.com>
2020-03-22 10:49:55 +0800

    Change getNearestOrder to return order with smallest time left and sort all packages upon init

--- Relentless/Relentless/Model/Order.swift
+++ Relentless/Relentless/Model/Order.swift
@@ -66,8 +66,7 @@ class Order: Hashable, Codable {
 
     /// Returns true if package matches items in order
     func checkPackage(package: Package) -> Bool {
-        let sortedPackage = package.sort()
-        return sortedPackage.items == items
+        package.items == items
     }
 
     /// Returns number of differences between the items in the package and the items in the order

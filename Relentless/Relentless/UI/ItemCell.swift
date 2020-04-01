48e706a5004f0d10206d31cda5131513ff3fbdcd
yaaanch <8232205+yaaanch@users.noreply.github.com>
2020-04-01 22:35:05 +0800

    Adds item assets and edits item cell

--- Relentless/Relentless/UI/ItemCell.swift
+++ Relentless/Relentless/UI/ItemCell.swift
@@ -11,8 +11,30 @@ import UIKit
 class ItemCell: UICollectionViewCell {
     // todo: change to UIImageView
     @IBOutlet private var textLabel: UILabel!
+    @IBOutlet private var background: UIImageView!
+
+    static let bookImage = UIImage(named: "book.png")
+    static let magazineImage = UIImage(named: "magazine.png")
+    static let unlitRobotImage = UIImage(named: "robot_lit.png")
+    static let litRobotImage = UIImage(named: "robot_unlit.png")
 
     func setItem(item: Item) {
+        setTextFor(item: item)
+        setBackgroundFor(item: item)
+    }
+
+    func setTextFor(item: Item) {
         textLabel.text = item.toString()
+    }
+
+    func setBackgroundFor(item: Item) {
+        switch item.category {
+        case .book:
+            background.image = ItemCell.bookImage
+        case .magazine:
+            background.image = ItemCell.magazineImage
+        case .bulb:
+            background.image = ItemCell.unlitRobotImage
+        }
     }
 }
